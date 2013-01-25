df6d68b77780dba52f622b65a3a8a58ff1c1293d
alex mejias <amejias101@qc.cuny.edu>
2013-01-25 09:24:59 -0500

    init added controller + routes layout is almost complete index view almost complete

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,16 @@
+class MainController < ApplicationController
+  def index
+  end
+
+  def why
+  end
+
+  def about
+  end
+
+  def pledge
+  end
+
+  def tips
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sg2Rails::Application.config.secret_token = '3ff0545c673c383cef1b3acb27c9990776ca9a2720f5a004f92355b857a4fe02fc6d74f948d3f3c6b2792a5718e1319935b2b9ee2c819fc5725e6c966e3d09da'
