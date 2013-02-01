f20a308f88f0465c27651a9ce339e53c7b81de57
alex mejias <amejias101@qc.cuny.edu>
2013-02-01 13:17:32 -0500

    put single tips in a partial new helper to display difficulty some styling changes

--- app/helpers/tips_helper.rb
+++ app/helpers/tips_helper.rb
@@ -1,2 +1,14 @@
 module TipsHelper
+	def difficulty_to_word(difficulty)
+		case difficulty
+		when 5
+			"investment"
+		when 3
+			"medium"
+		when 1
+			"easy"
+		else
+			"other"
+		end
+	end
 end
